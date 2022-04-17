5dfafa42564683b8b886b66a6b28e5aa31fd0647
KimberleySchubert <kimberley.schubert@gmail.com>
2022-04-16 22:58:33 -0400

    04-16-22 eod - #2 tweaks + hamburger

---
+++ javascript/script.js
@@ -0,0 +1,6 @@
+// Hamburger Menu JS
+function menuOnClick() {
+    document.getElementById("hamburg").classList.toggle("change");
+    document.getElementById("hamNav").classList.toggle("change");
+    document.getElementById("hamMenuBGND").classList.toggle("changeBGND");
+}
